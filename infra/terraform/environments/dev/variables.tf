a409218d627f97a9e888a9a7508aef86c83739cd
Derick Omondi <derickomondi591@gmail.com>
2026-07-26 16:38:27 +0300

    Update variables.tf

--- infra/terraform/environments/dev/variables.tf
+++ infra/terraform/environments/dev/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region where dev infrastructure will be provisioned."
   type        = string
-  default     = "af-south-1"
+  default     = "eu-west-1"
 }
 
 variable "availability_zone" {
